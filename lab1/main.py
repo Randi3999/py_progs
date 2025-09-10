3c16910c3bfacca885eaa03813771a48ba2e4fbd
Randi3999 <ssklimov11@gmail.com>
2025-09-10 12:52:43 +0500

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -1,10 +1,1 @@
-print("Привет! Это игра Камень, ножницы, бумага!")
-
-txt = ['Камень','Ножницы','Бумага']
-# print(choise(txt))
-Value = str(input("Введите ваш ход: "))
-n = 0
-while(n == 0):
-    KompValue = txt.choise
-    if Value == "Камень" and KompValue == "Но":
 
